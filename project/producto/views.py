ae23476f993b641b86e6799826f213d685f1229d
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-04-29 21:44:16 -0300

    feat(producto): CRUD ProductoCategoria

--- project/producto/views.py
+++ project/producto/views.py
@@ -4,16 +4,18 @@ from . import forms, models
 
 
 def home(request):
-    consulta = request.GET.get("consulta", None)
+    return render(request, "producto/index.html")
 
+
+def productocategoria_list(request):
+    consulta = request.GET.get("consulta", None)
     if consulta:
         print(consulta)
         query = models.ProductoCategoria.objects.filter(nombre__icontains=consulta)
     else:
         query = models.ProductoCategoria.objects.all()
-
     context = {"productos": query}
-    return render(request, "producto/index.html", context)
+    return render(request, "producto/productocategoria_list.html", context)
 
 
 def productocategoria_create(request):
@@ -25,3 +27,28 @@ def productocategoria_create(request):
     else:  # request.method == "GET"
         form = forms.ProductoCategoriaForm()
     return render(request, "producto/productocategoria_create.html", context={"form": form})
+
+
+def productocategoria_detail(request, pk: int):
+    query = models.ProductoCategoria.objects.get(id=pk)
+    return render(request, "producto/productocategoria_detail.html", {"producto": query})
+
+
+def productocategoria_update(request, pk: int):
+    query = models.ProductoCategoria.objects.get(id=pk)
+    if request.method == "POST":
+        form = forms.ProductoCategoriaForm(request.POST, instance=query)
+        if form.is_valid:
+            form.save()
+            return redirect("producto:productocategoria_list")
+    else:  # request.method == "GET"
+        form = forms.ProductoCategoriaForm(instance=query)
+    return render(request, "producto/productocategoria_update.html", context={"form": form})
+
+
+def productocategoria_delete(request, pk: int):
+    query = models.ProductoCategoria.objects.get(id=pk)
+    if request.method == "POST":
+        query.delete()
+        return redirect("producto:productocategoria_list")
+    return render(request, "producto/productocategoria_delete.html", context={"producto": query})
