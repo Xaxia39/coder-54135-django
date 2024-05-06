0a3e884d4a49306d4dc44c446c857cafe02069b4
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-05-06 20:59:29 -0300

    feat(producto): busqueda

--- project/producto/views.py
+++ project/producto/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
@@ -23,9 +24,17 @@ def home(request):
 
 class ProductoCategoriaList(ListView):
     model = models.ProductoCategoria
+
     # context_object_name = "productos"
     # template_name = "producto/productocategoria___list.html"
 
+    def get_queryset(self) -> QuerySet:
+        if self.request.GET.get("consulta"):
+            consulta = self.request.GET.get("consulta")
+            object_list = models.ProductoCategoria.objects.filter(nombre__icontains=consulta)
+        else:
+            object_list = models.ProductoCategoria.objects.all()
+        return object_list
 
 # CREATE
 # def productocategoria_create(request):
