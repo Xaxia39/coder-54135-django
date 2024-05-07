9f06dfa6eacd6a200ef10663fdddb567e383f7b6
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-05-06 22:11:45 -0300

    feat: login & logout

--- project/core/views.py
+++ project/core/views.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 
-from . import models
+from .forms import CustomAuthenticationForm
 
 
 def home(request):
     return render(request, "core/index.html")
+
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
